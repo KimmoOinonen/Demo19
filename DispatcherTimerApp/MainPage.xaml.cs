20c3e29b495f79cbacdd54a75ee04f4f51049659
Kimmo Oinonen <kimmo.oinonen94@gmail.com>
2016-03-16 15:23:54 +0200

    Initial commit.

---
+++ DispatcherTimerApp/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace DispatcherTimerApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private DispatcherTimer dispatcherTimer;
+        private int timesTicked = 0;
+        private int timesToTick = 10;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += DispatcherTimer_Tick;
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            TimerLog.Text = "Start()";
+            dispatcherTimer.Start();
+        }
+
+        private void DispatcherTimer_Tick(object sender, object e)
+        {
+            timesTicked++;
+            TimerLog.Text += "DispatcherTimer_Tick called!" + Environment.NewLine;
+            // max 10 times
+            if (timesTicked >= timesToTick)
+            {
+                TimerLog.Text += "Stop()";
+                dispatcherTimer.Stop();
+            }
+        }
+    }
+}
